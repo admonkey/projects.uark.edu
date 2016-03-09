726f35330a12063711c138810ab8be3e71af04b7
mysmartbaobao <mysmartbaobao@gmail.com>
2016-03-09 15:04:59 -0600

    check if vote already exists

--- vote.ajax.php
+++ vote.ajax.php
@@ -1,53 +1,38 @@
 <?php
 
-// we don't need this block since all database connections are handled in global header
-/*
-//save results to mysql
-$user = "naliu";
-$pass = "_YvYuoX09ADpAyIO​";
-$db   = "projects_naliu";
-//? Which host?
-$host = "localhost";
-*/
-
 // require header for db connections
 $include_mysqli = true;
 require_once("_resources/header.inc.php");
 
-// NEED TO SANITIZE USER INPUT!
+// SANITIZE USER INPUT!
 $content_key = $_GET["content_key"];
 $vote_value = $_GET["vote_value"];
-
-// we don't need this since it is auto-generated on table
-//$vote_creation_time = time();
 
 if(!empty($_GET["user_key"]))
   $user_key = $_GET["user_key"];
 
-// removed vote_creation_time
-//$sql = "INSERT INTO Votes (user_key, content_key, vote_value, vote_creation_time) VALUES ($user_key, $content_key, $vote_value, $vote_creation_time)";
-$sql = "INSERT INTO Votes (user_key, content_key, vote_value) VALUES ($user_key, $content_key, $vote_value)";
+// Check if vote already exists.
 
+$sql = "SELECT vote_value FROM Votes WHERE user_key = $user_key AND content_key = $content_key";
 
-// we don't need this block since all database connections are handled in global header
-/*
-mysql_connect( $host, $user, $pass ) or die("Issue with connecting");
-mysql_select_db( $db );
-mysql_query($sql);
-mysql_close();
-*/
-
-// this is the old deprecated mysql functions, do not use these as they are insecure
-//$result = mysql_query($sql);
-
-// use the mysqli functions: http://php.net/manual/en/book.mysqli.php
 if( !empty($mysqli_connected) ){
   $result = $mysqli_connection->query($sql) or die($mysqli_connection->error);
 }
 
-
-
-
+// Update the vote if it already exists.
+if(!empty($result)){
+  $sql = "UPDATE Votes SET vote_value = $vote_value WHERE user_key = $user_key AND content_key = $content_key"; 
+  if( !empty($mysqli_connected) ){
+	$result = $mysqli_connection->query($sql) or die($mysqli_connection->error);
+  }
+}
+// Insert a new record if the vote if it does not exist.
+else {
+	$sql = "INSERT INTO Votes (user_key, content_key, vote_value) VALUES ($user_key, $content_key, $vote_value)";
+	if( !empty($mysqli_connected) ){
+	$result = $mysqli_connection->query($sql) or die($mysqli_connection->error);
+	}
+}
 
 
 // while debugging, go ahead and print out all the values,
@@ -74,8 +59,6 @@ while($row = $result->fetch_array(MYSQLI_NUM)){
 }
 // close table
 echo "</tbody></table>";
-
-
 
 
 // require footer to close db connections
