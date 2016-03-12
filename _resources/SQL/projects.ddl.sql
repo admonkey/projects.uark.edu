bd90d9431accdb9384cf9e6c2825260ec2788911
Jeff Puckett <jeff@jeffpuckett.com>
2016-03-12 13:43:14 -0600

    remove test

--- _resources/SQL/projects.ddl.sql
+++ _resources/SQL/projects.ddl.sql
@@ -452,29 +452,4 @@ this_procedure:BEGIN
 
 END $$
 
-CREATE PROCEDURE create_vote (
-  IN p_user_key INT
-)
-
-this_procedure:BEGIN
-
-  DECLARE existing_vote_value INT DEFAULT NULL;
-  DECLARE valid_number INT DEFAULT NULL;
-  DECLARE existing_vote_time TIMESTAMP DEFAULT NULL;
-  DECLARE user_key_check    INT DEFAULT NULL;
-  DECLARE content_key_check INT DEFAULT NULL;
-  DECLARE content_status    INT DEFAULT NULL;
-  
-  -- validate user_key
-  -- validate content_key
-  -- validate vote_value value (either -2, -1, or 1)
-  -- count the user_key and content_key (should be 1) and exit the stored procedure if not.
-  
-  
-    select user_key into user_key_check from Users where user_key = p_user_key;
-        select user_key_check;
-
-END $$
-
-
 DELIMITER ;
