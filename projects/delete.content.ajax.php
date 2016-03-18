2715fb9fffb1519da4b7bbd55f7b6928e72d4e6f
Jeff Puckett <jeff@jeffpuckett.com>
2016-03-17 21:58:05 -0500

    add stub for delete.content.ajax

--- projects/delete.content.ajax.php
+++ projects/delete.content.ajax.php
@@ -1,53 +1,47 @@
 <div>
 <?php
+$include_mysqli = true;
+require_once("_resources/header.inc.php");
 
-if ( !empty($_GET["message_id"]) && is_numeric($_GET["message_id"]) ){
-  $message_id = $_GET["message_id"];
-} else {
-  echo "<p class='bg-danger text-danger'>ERROR: Invalid Message ID</p>";
-  die();
-}
+if (valid_positive_integer(@$_GET["content_key"])) {
+  $content_key = $_GET["content_key"];
+  if (!empty($_SESSION["user_key"])){
+    $user_key = $_SESSION["user_key"];
+      if( !empty($mysqli_connected) ){
+      // BEGIN validation wrapper
 
-include_once('_resources/credentials.inc.php');
-$include_mysqlo = true;
-require_once('_resources/header.inc.php');
 
-sec_session_start();
-if (empty($_SESSION["user_key"])){
-  echo "<p class='bg-danger text-danger'>ERROR: Not Logged In</p>";
-  die();
-} else {
-  $user_key = $_SESSION["user_key"];
-}
 
 if (isset($_GET["restore"])) $deleted = 0;
 else $deleted = 1;
 
-?>
+/*// update to mysqli
+$sql="CALL Forum_proc_Delete_Message($user_key, $content_key, $deleted)";
+$result = mysql_query($sql) or die(mysql_error());
+*/
+if ($deleted == 1)
+  echo "
+    <div>
+      <p class='text-danger'>Message Deleted</p>
+      <p><a href='javascript:void(0)' onclick='delete_message($content_key + \"&restore\", $(this), true)'><label class='label label-warning'>Undo</label></a></p>
+    </div>
+  ";
 
-<?php
 
-if( !empty($mysqlo_connected) ){
 
-    $sql="CALL Forum_proc_Delete_Message($user_key, $message_id, $deleted)";
-    $result = mysql_query($sql) or die(mysql_error());
-
-    if ($deleted == 1)
-      echo "
-      <div>
-	<p class='text-danger'>Message Deleted</p>
-	<p><a href='javascript:void(0)' onclick='delete_message($message_id + \"&restore\", $(this), true)'><label class='label label-warning'>Undo</label></a></p>
-      </div>";
-
+    // END validation wrapper
+    } else {
+      // help connecting to database
+      echo "<p class='bg-danger text-danger'>ERROR: Not Connected to Database</p>";
+      include("$path_real_root/_resources/SQL/database.help.inc.html");
+    }
+  } else {
+    echo "<p class='bg-danger text-danger'>ERROR: Not Logged In</p>";
+  }
 } else {
-
-    // help connecting to database
-    echo "<p class='bg-danger text-danger'>ERROR: Not Connected to Database</p>";
-    include("$path_real_root/_resources/SQL/database.help.inc.html");
-
+  echo "<p class='bg-danger text-danger'>ERROR: Invalid Content ID</p>";
 }
 
+require_once("_resources/footer.inc.php");
 ?>
-
-<?php require_once('_resources/footer.inc.php');?>
 </div>
