47d86b7174da9d3ab8e7b5e4cf81ef1a68a9ad2a
Jeff Puckett <jeff@jeffpuckett.com>
2016-03-24 22:37:05 -0500

    authenticate with $_SESSION and refactor validation wrapper

--- projects/vote.ajax.php
+++ projects/vote.ajax.php
@@ -4,31 +4,42 @@
 $include_mysqli = true;
 require_once("_resources/header.inc.php");
 
-// get the values
-$content_key = $_GET["content_key"];
-$vote_value = $_GET["vote_value"];
+if(!empty($_SESSION["user_key"])){
+  $user_key = $_SESSION["user_key"];
+  if (valid_positive_integer(@$_GET["content_key"])){
+    $content_key = $_GET["content_key"];
+    if(!empty($_GET["vote_value"])){
+      $vote_value = $_GET["vote_value"];
+      if ($vote_value == (-2) || $vote_value == (-1) || $vote_value == 1) {
+      // BEGIN validation wrapper
 
-if(!empty($_GET["user_key"]))
-  $user_key = $_GET["user_key"];
 
-// sanitize the values
-if (valid_positive_integer($content_key) && valid_positive_integer($user_key)) {
-
-	if ($vote_value == (-2) || $vote_value == (-1) || $vote_value == 1) {
-		// call the sql precedue to do the voting
-		$sql = "CALL create_vote(?,?,?)";
-		// Call create_vote
-		if (!($stmt = $mysqli_connection->prepare($sql))) {
-			echo "Prepare failed: (" . $mysqli_connection->errno . ") " . $mysqli_connection->error;
-		}
-		else {
-			$stmt->bind_param('iii', $user_key, $content_key, $vote_value);
-			if (!$stmt->execute()) {
-				echo "Execute failed: (" . $stmt->errno . ") ";
-			}
-		}
-	}
+// call the sql precedue to do the voting
+$sql = "CALL create_vote(?,?,?)";
+if (!($stmt = $mysqli_connection->prepare($sql))) {
+  echo "Prepare failed: (" . $mysqli_connection->errno . ") " . $mysqli_connection->error;
+} else {
+  $stmt->bind_param('iii', $user_key, $content_key, $vote_value);
+  if (!$stmt->execute()) {
+    echo "Execute failed: (" . $stmt->errno . ") ";
+  }
 }
+
+
+      // END validation wrapper
+      } else {
+        echo "ERROR: invalid vote value!";
+      }
+    } else {
+      echo "ERROR: no vote!";
+    }
+  } else {
+    echo "ERROR: invalid content key!";
+  }
+} else {
+  echo "ERROR: not logged in!";
+}
+
 
 // require footer to close db connections
 require_once("_resources/footer.inc.php");
