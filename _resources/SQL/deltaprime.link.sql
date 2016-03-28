a4bc022ca7a59c90dc7ad6aa43df5e635df838e6
Jeff Puckett <jeff@jeffpuckett.com>
2016-03-28 17:56:37 -0500

    progress

--- _resources/SQL/deltaprime.link.sql
+++ _resources/SQL/deltaprime.link.sql
@@ -15,3 +15,39 @@ CREATE TABLE IF NOT EXISTS Link_DeltaPrimeComments (
 
 DELETE FROM Users WHERE user_key = -2;
 INSERT INTO Users (email,username, user_key) VALUES ('deltaprime','deltaprime', -2);
+
+DROP FUNCTION IF EXISTS import_new_deltaprime_project;
+DELIMITER $$
+CREATE PROCEDURE import_new_deltaprime_project (
+  p_content_creation_time TIMESTAMP,
+  p_content_edited_time TIMESTAMP,
+  p_content_title VARCHAR(100),
+  p_content_value VARCHAR(1000),
+  p_Project_Id INT
+)
+this_procedure:BEGIN
+
+  DECLARE new_content_key INT DEFAULT NULL;
+
+  INSERT INTO Content (
+    content_title,
+    content_value,
+    project_key,
+    content_creation_time,
+    content_createdby_user_key,
+    content_edited_time,
+    content_editedby_user_key
+  ) VALUES (
+    p_content_title,
+    p_content_value,
+    NULL,
+    p_content_creation_time,-2,
+    p_content_edited_time,-2
+  );
+  
+  SET new_content_key = LAST_INSERT_ID();
+  
+  INSERT INTO Link_DeltaPrimeProjects VALUES (p_Project_Id,new_content_key);
+
+END $$
+DELIMITER ;
--- projects/deltaprime.php
+++ projects/deltaprime.php
@@ -20,14 +20,18 @@ if( !empty($mysqli_connected) ){
       Project_Id INT,
       project_key INT
     );
-    INSERT INTO @Link_DeltaPrimeProjects VALUES 
   ";
   $result = $mysqli_connection->query("SELECT * FROM Link_DeltaPrimeProjects") or die($mysqli_connection->error);
-  while ($row = $result->fetch_assoc()){
-      $sql_Link_DeltaPrimeProjects .= "($row[Project_Id],$row[project_key]),";
+  if($result->num_rows > 0){
+      $sql_Link_DeltaPrimeProjects .= "
+        INSERT INTO @Link_DeltaPrimeProjects VALUES 
+      ";
+      while ($row = $result->fetch_assoc()){
+          $sql_Link_DeltaPrimeProjects .= "($row[Project_Id],$row[project_key]),";
+      }
+      // trim trailing comma and end statement;
+      $sql_Link_DeltaPrimeProjects = rtrim($sql_Link_DeltaPrimeProjects, ",") . ";";
   }
-  // trim trailing comma and end statement;
-  $sql_Link_DeltaPrimeProjects = rtrim($sql_Link_DeltaPrimeProjects, ",") . ";";
   
   // get deltaprime projects, joined with link keys
   $mssql_query = "
@@ -58,55 +62,43 @@ if( !empty($mysqli_connected) ){
     $mssql_password
   ) or die("could not connect: " . odbc_errormsg());
 
-  $mssql_result = odbc_exec($mssql_connection, $mssql_query);
+  $mssql_result = odbc_exec($mssql_connection, $mssql_query) or die($mysqli_connection->error);
 
   // print
-  while (odbc_next_result($mssql_result)){
-    //odbc_result_all($mssql_result, "border=1");
+  do {
+    odbc_result_all($mssql_result, "border=1");
     
-    $sql_insert_new_projects = "
-      INSERT INTO Content (
-        content_title,
-        content_value,
-        project_key,
-        content_creation_time,
-        content_createdby_user_key,
-        content_edited_time,
-        content_editedby_user_key
-      ) VALUES
-    ";
-    
+    //$sql_insert_new_projects = "";
     $sql_update_old_projects = "";
     $sql_delete_old_projects = "";
       
     while ($row = odbc_fetch_array($mssql_result)){
 
       // null time if not edited
-      if(empty($row["content_edited_time"])) $content_edited_time = "NULL";
+      if(empty($row["content_edited_time"])) $content_edited_time = null;
       else $content_edited_time = "'$row[content_edited_time]'";
 
       // if new project
       if (empty($row["project_key"])){
-        $sql_insert_new_projects .= "
-          INSERT INTO Content (
-            content_title,
-            content_value,
-            project_key,
-            content_creation_time,
-            content_createdby_user_key,
-            content_edited_time,
-            content_editedby_user_key
-          ) VALUES (
-            '$row[content_title]',
-            '$row[content_value]',
-            $row[project_key],
-            '$row[content_creation_time]',-2,
-            $content_edited_time,-2
-          );
-          
-          -- ADD LINK HERE
-          --INSERT INTO Link_DeltaPrimeProjects VALUES (,);
+        $sql_insert_new_projects = "
+          CALL import_new_deltaprime_project(?,?,?,?,?);
         ";
+        //die($sql_insert_new_projects);
+        if (!($stmt = $mysqli_connection->prepare($sql_insert_new_projects))) {
+          echo "Prepare failed: (" . $mysqli_connection->errno . ") " . $mysqli_connection->error;
+        } else {
+          $stmt->bind_param('ssssi', $row["content_creation_time"], $content_edited_time, $row["content_title"], $row["content_value"], $row["Project_Id"]);
+          if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") ". $stmt->error;
+          } else {
+            /*
+            $stmt->store_result();
+            $stmt->bind_result($response);
+            $stmt->fetch();
+            */
+            echo "inserted<br/>";
+          }
+        }
       }
       
       // else if deleted
@@ -131,12 +123,22 @@ if( !empty($mysqli_connected) ){
     
     }
   
-  }
+  } while (odbc_next_result($mssql_result));
     
-
   // close
   odbc_close_all();
   
+  // update MySQL
+  //if(!empty($sql_insert_new_projects)) echo "$sql_insert_new_projects";
+    //$result = $mysqli_connection->query("$sql_insert_new_projects") or die($mysqli_connection->error);
+  //else echo "no new projects<br/>";
+  if(!empty($sql_update_old_projects)) echo "$sql_update_old_projects";
+    //$result = $mysqli_connection->query("$sql_update_old_projects") or die($mysqli_connection->error);
+  else echo "no updated projects<br/>";
+  if(!empty($sql_delete_old_projects)) echo "$sql_delete_old_projects";
+    //$result = $mysqli_connection->query("$sql_delete_old_projects") or die($mysqli_connection->error);
+  else echo "no deleted projects<br/>";
+
 } else {
   // help connecting to database
   echo "<p class='bg-danger text-danger'>ERROR: Not Connected to Database</p>";
